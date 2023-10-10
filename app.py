30868fed87ba3188063e08fd30d1f8aa0e71e23d
Alfin Gunawan <alfingunawan003@gmail.com>
2023-10-10 13:57:31 +0700

    Flask setup for deployment

--- app.py
+++ app.py
@@ -1,8 +1,19 @@
+
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
-client = MongoClient('mongodb+srv://Alfin:alfingun@cluster0.wp4rymm.mongodb.net/?retryWrites=true&w=majority')
-db = client.dbbucket
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+
+db = client[DB_NAME]
 
 app = Flask(__name__)
 
@@ -20,7 +31,6 @@ def bucket_post():
         'bucket': bucket_receive,
         'done' : 0
     }
-
     db.bucket.insert_one(doc)
     return jsonify({'msg': 'POST /bucket request!'})
 
